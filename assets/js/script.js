988f173d13d9ede72b2b331569359649e84aadf2
LeoVergaraS <leo.vergara@usach.cl>
2023-06-19 03:08:10 -0400

    cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// Entrada: No tiene
+// Salida: Json, retorna los primeros 20 pokemones
+// Descripción: Función que hace un GET de la API de pokemonapi
+//              de los 20 primeros pokemones
 const getPokemones = async () => {
     try{
         const url = 'https://pokeapi.co/api/v2/pokemon/';
@@ -9,6 +13,10 @@ const getPokemones = async () => {
     }
 };
 
+// Entrada: String, Url de donde se realizara la consulta
+// Salida: Json, retorna los datos de un pokemon
+// Descripción: Función que hace un GET de la API de pokemonapi
+//              de un pokemon en especifico
 const getPokemon = async (url) => {
     try{
         const res = await fetch(url);
@@ -19,6 +27,10 @@ const getPokemon = async (url) => {
     }
 };
 
+// Entrada: String, palabra a convertir
+// Salida: String, palabra con la primera letra en mayuscula
+// Descripción: Función que convierte la primera letra de una
+//              palabra en mayuscula
 const mayuscula = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
 };
@@ -32,9 +44,12 @@ const cardHTML = (pokemon) => {
             </div>`)
 };
 
-let pok = document.getElementById('pokemones');
-const content = pok.innerHTML;
+// Entrada: No tiene
+// Salida: No tiene
+// Descripción: Función que crea las cards de los pokemones
 const PokemonHTML = async () => {
+    let pok = document.getElementById('pokemones');
+    const content = pok.innerHTML;
     const pokemones = await getPokemones();
     pokemones.forEach(async (pkmn) => {
         const data = await getPokemon(pkmn.url);
